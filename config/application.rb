a3f7787d6105a76c26a199327ad1d7fe5d7ba5c3
a.ito <10333049+BoxPistols@users.noreply.github.com>
2024-09-28 20:53:03 +0900

    Remove Webpacker and update asset configuration

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module Mysite
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -23,5 +24,7 @@ module Mysite
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # config.assets.paths << Rails.root.join("app", "assets", "builds")
   end
 end
